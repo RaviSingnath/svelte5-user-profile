688cefcb0a99a18f439a507a3d6a0f93bb837e3b
RaviSingnath <ravisingnath@gmail.com>
2025-04-05 15:06:05 +0530

    Profile page

---
+++ src/routes/profile/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import Profile from '@components/profile/Profile.svelte';
+	import PersonalInformation from '@components/profile/PersonalInformation.svelte';
+	import Notifications from '@components/profile/Notifications.svelte';
+
+	let { children } = $props();
+</script>
+
+<div class="bg-gray-50">
+	<div class="mx-auto max-w-7xl py-12 sm:px-6 sm:py-24 lg:px-8">
+		<div class="divide-y divide-gray-900/10">
+			<Profile />
+			<PersonalInformation />
+			<Notifications />
+		</div>
+	</div>
+</div>
